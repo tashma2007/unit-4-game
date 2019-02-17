0d4f35d85254eb35496805c2ca7f954c721ec7e6
Natasha Henry <natashahenry@Natashas-MacBook-Pro.local>
2019-02-17 00:11:23 -0500

    adding javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,17 @@
-var randomNumber;
-var wins
-var losses
+$(document).ready(function() {
+    var randombox = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+
+    $(".randomNumber").html(randombox);
+
+    $("#button").on("click", function() {
+
+    console.log(#button);
+    
+    });
+
+
+
+
+
+
+});
